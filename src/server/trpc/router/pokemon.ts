0985c74afef2d8e7c5f17589f3b8abf2fb5a729f
KassenBoyaubay <kassen.boyaubay@alumni.nu.edu.kz>
2022-12-30 01:25:00 +0100

    populated all pokemons, add pokemon table, remove ts-node

--- src/server/trpc/router/pokemon.ts
+++ src/server/trpc/router/pokemon.ts
@@ -25,4 +25,22 @@ export const pokemonRouter = router({
             })
             return { success: true, vote: voteInDb };
         })
+    ,
+    populatePokemons: publicProcedure
+        .mutation(async () => {
+            const pokeApi = new PokemonClient();
+
+            const allPokemon = await pokeApi.listPokemons(0, 493);
+
+            const formattedPokemon = allPokemon.results.map((p, index) => ({
+                id: index + 1,
+                name: (p as { name: string }).name,
+                spriteUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+            }))
+
+            const creation = await prisma.pokemon.createMany({
+                data: formattedPokemon
+            })
+            return { success: true, pokemon: creation };
+        })
 });
